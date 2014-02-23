6d02cea2c01d717f2c60cccada0f997ced20fc6f
Alexandr Andreev <alexandr.andreev@simbirsoft.com>
2014-02-23 13:55:38 +0400

    Initial commit

---
+++ VSDesktops/DesktopManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.CommandBars;
+
+namespace VSDesktops
+{
+    class DesktopManager
+    {
+        public DesktopManager(DTE2 applicationObject)
+        {
+            _applicationObject = applicationObject;
+        }
+
+        private const int DesktopCount = 3;
+        private readonly IDictionary<string, Command> buttons = new Dictionary<string, Command>();
+
+        public void CreateButtons(CommandBars cmd, Commands2 commands, AddIn _addInInstance)
+        {
+            var contextGUIDS = new object[] { };
+            var bar = cmd["Standard"];
+            for (int i = 0; i < DesktopCount; i++)
+            {
+                var commandName = string.Format("Desk{0}", i);
+                var commandText = string.Format("Desk {0}", i + 1);
+                var command = commands.AddNamedCommand2(_addInInstance, commandName, commandText, string.Empty, true, 0, ref contextGUIDS);
+                command.AddControl(bar, bar.Controls.Count);
+                buttons.Add(commandName, command);
+            }
+
+        }
+
+        public bool HandleExec(string commandName)
+        {
+            if (!buttons.ContainsKey(commandName))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+        //private void SwitchDesktop()
+        //{
+        //    var windows =
+        //        _applicationObject.Windows.Cast<Window>()
+        //            .Where(w => w.Type == vsWindowType.vsWindowTypeDocument);
+        //    foreach (var window in windows)
+        //    {
+        //        window.Visible = _isFirstDesktop;
+        //    }
+        //    _isFirstDesktop = !_isFirstDesktop;
+        //}
+
+        private readonly DTE2 _applicationObject;
+
+    }
+}
